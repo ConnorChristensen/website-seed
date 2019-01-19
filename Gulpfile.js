4e94493b625dc34f990dcffd5574be3ecfe36c87
Connor Christensen <chriconn@oregonstate.edu>
2019-01-18 18:09:29 -0800

    Install image compression and file inclusion

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,7 @@ var plugin = require("gulp-load-plugins")({
    pattern: ['gulp-*', 'gulp.*'],
    replaceString: /\bgulp[\-.]/
 });
+var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
 var config      = require('config.json')('./config.json');
 var root        = config.root + "/";
@@ -13,7 +14,8 @@ var paths = {
    start: {
       html: root + config.folders.html + "/*.html",
       css:  root + config.folders.css  + "/*.styl",
-      js:   root + config.folders.js   + "/*.js"
+      js:   root + config.folders.js   + "/*.js",
+      images: root + config.folders.images + "/**/*"
    }
 }
 ///////////
@@ -22,19 +24,30 @@ var paths = {
 // move all the html files to the destination folder
 gulp.task('html', function() {
    return gulp.src(paths.start.html)  // get our html files
-      .pipe(plugin.flatten())         // flatten the folder hierarchy
+      .pipe(fileinclude({             // @@include('') allows file inclusion
+        prefix: '@@',
+        basepath: '@file'
+      }))
       .pipe(gulp.dest(destination));  // send them over to the destination folder
 });
 
 // get all js files and put them in the destination folder
 gulp.task('js-dev', function() {
    return gulp.src(paths.start.js)    // get our js files
+       .pipe(fileinclude({            // @@include('') allows file inclusion
+         prefix: '@@',
+         basepath: '@file'
+       }))                            // inject json files
       .pipe(gulp.dest(destination));  // move them to our destination folder
 });
 
 gulp.task('js-prod', function() {
    return gulp.src(paths.start.js)    // get our js files
-      .pipe(plugin.uglify())
+       .pipe(fileinclude({            // @@include('') allows file inclusion
+         prefix: '@@',
+         basepath: '@file'
+       }))                            // inject json files
+       .pipe(plugin.uglify())
       .pipe(gulp.dest(destination));  // move them to our destination folder
 });
 
@@ -45,9 +58,16 @@ gulp.task('css', function() {
       .pipe(gulp.dest(destination));  // send them over to the destination folder
 });
 
-gulp.task('images', function() {
+gulp.task('images-dev', function() {
    // get our images folder and send them over to an images folder in destination
-   return gulp.src(root + paths.start.images + '/**/*')
+   return gulp.src(paths.start.images)
+      .pipe(gulp.dest(destination + 'images'));
+});
+
+gulp.task('images-prod', function() {
+   // get our images folder and send them over to an images folder in destination
+   return gulp.src(paths.start.images)
+      .pipe(plugin.image())
       .pipe(gulp.dest(destination + 'images'));
 });
 
@@ -68,6 +88,7 @@ gulp.task('watch', function() {
    gulp.watch(root + '**/*.styl', gulp.series('css'));
    gulp.watch(root + '**/*.html', gulp.series('html'));
    gulp.watch(root + '**/*.js',   gulp.series('js-dev'));
+   gulp.watch(paths.start.images, gulp.series('images-dev'));
 });
 
 // serve up the page on a browser
@@ -87,18 +108,15 @@ gulp.task('serve', function() {
 ////////////////
 // simply builds the dist folder. Good for testing the build
 // process without serving the site
-gulp.task('build', gulp.parallel(
-  'html', 'css', 'images'
-));
+gulp.task('build', gulp.parallel('html', 'css'));
 
 // build, format the socket.io url to localhost and serve the site
 gulp.task('dev', gulp.series(
-   'build',
-   'js-dev',
+   gulp.parallel('build', 'js-dev', 'images-dev'),
    gulp.parallel('watch', 'serve')
 ));
 
-gulp.task('prod', gulp.parallel('build', 'js-prod'));
+gulp.task('prod', gulp.parallel('build', 'images-prod', 'js-prod'));
 
 // build for local by default
 gulp.task('default', gulp.parallel('dev'));
